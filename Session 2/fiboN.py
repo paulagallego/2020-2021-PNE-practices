c27cc9e8b66d505d9b5be2637a1f0e4267c94c0a
paulagallego <p.gallego.2020@alumnos.urjc.es>
2021-02-17 17:31:16 +0100

    Create fiboN.py

---
+++ Session 2/fiboN.py
@@ -0,0 +1,12 @@
+def fibon(n):
+    new_list = [0,1]
+    x = 0
+    y = 1
+    for i in range(1, n-1):
+        sum = x+y
+        x = y
+        y = sum
+        new_list.append(sum)
+    return new_list
+print('the first 11 fibonacci numbers are: ', fibon(11))
+print('the first 15 fibonacci numbers are: ', fibon(15))
--- Session 2/fibonacci.py
+++ Session 2/fibonacci.py
@@ -1,7 +1,7 @@
 series = [0, 1, 1]
 a = 1
 b = 1
-for i un range(1,9):
+for i in range(1,9):
     c = b+a
     series.append(c)
     a=b
