f2634098a15680b8c3ceffef48cb1971031bf272
paulagallego <p.gallego.2020@alumnos.urjc.es>
2021-02-24 12:20:44 +0100

    head.py

--- Session 4/head.py
+++ Session 4/head.py
@@ -1,13 +1,9 @@
 #Write a python program that opens the file RNU6_269P.txt and prints only its head
 
 from pathlib import Path
-FILENAME = input('Introduce a valid filename')
-file_contents = Path(FILENAME).readline()
-head = 0
-for line in file_contents:
-    if head == 0:
-        x = file_contents.split('\n')
-        head +=1
-        print('first line of the', FILENAME, 'file:', '\n', x )
-    else:
-        break
+FILENAME = "RNU6_269P.txt"
+file_contents = Path(FILENAME).read_text()
+
+x = file_contents.split('\n', 1)
+
+print('First line of the RNU6_269P.txt file: ', '\n', x[0])
