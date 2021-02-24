7337aaa0559fbf1e4a691531a72ccbea85a7956b
paulagallego <p.gallego.2020@alumnos.urjc.es>
2021-02-24 19:16:52 +0100

    until ex5 errors fixed

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -9,6 +9,6 @@ for gene in gene_list:
     sequence = Seq0.seq_read_fasta(GENE_FOLDER + gene + '.txt') #we need to use GENE_FOLDER in order to obtain the sequences (as ex1, ex2 are in directory P0 and sequences are in sequences directory)
     print('Gene', gene)
     for base in base_list:
-        print(base + ":", Seq0.seq_count_base(sequence, base)
+        print(base + ":", Seq0.seq_count_base(sequence, base))
 
 
